e8aaab5bff797052d54b5bf0f3b58e73422eb4c7
Dmitriy Migorenko <migorenko.dmitry@yandex.ru>
2020-08-31 22:05:37 +0300

    Money Transfer added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args){
+        int CurrentBalance = 2_000_000_000;
+        int SumTransfer = 500_000_000;
+        int TotalBalance = CurrentBalance + SumTransfer;
+        System.out.println(TotalBalance);
+    }
+}
