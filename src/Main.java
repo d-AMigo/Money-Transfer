c088e59998cfbde88266c73f8e359864718f105c
Dmitriy Migorenko <migorenko.dmitry@yandex.ru>
2020-09-01 18:21:24 +0300

    report.md added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         int CurrentBalance = 2_000_000_000;
-        int SumTransfer = 500_000_000;
+        int SumTransfer = -500_000_000;
         int TotalBalance = CurrentBalance + SumTransfer;
         System.out.println(TotalBalance);
     }
